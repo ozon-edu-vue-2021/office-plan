9a1ce7ac0735dcf0adc18c6d971823c78ca513a8
Marina Potovichenko <mpotovichenko@ozon.ru>
2021-09-25 14:03:12 +0300

    Added map layout

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,17 +1,47 @@
 <template>
     <div class="map">
-        Map
+        <h3>Карта офиса</h3>
+
+        <div class="map-root">
+            <MapSVG />
+        </div>
     </div>
 </template>
 
 <script>
+import MapSVG from '@/assets/map.svg';
+
 export default {
+    components: {
+        MapSVG,
+    },
 };
 </script>
 
 <style scoped>
 .map {
+    height: 100%;
     width: 100%;
     padding: 24px;
+    overflow: hidden;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+}
+
+.map-root {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    box-sizing: border-box;
+}
+
+h3 {
+    margin-top: 0px;
+}
+
+>>> svg {
+    height: 100%;
+    width: 100%;
 }
 </style>

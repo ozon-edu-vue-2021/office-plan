bc6471d428c3412866bf7441f1d9af211ef49fa2
Marina Potovichenko <mpotovichenko@ozon.ru>
2021-09-26 15:55:12 +0300

    Added d3 and drew tables

--- src/components/LegendItem.vue
+++ src/components/LegendItem.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="legend-item">
-    <div class="legend-item__marker">
-      <div class="marker" :style="{ '--marker-color': color }" />
+    <div class="legend-item">
+        <div class="legend-item__marker">
+            <div
+                class="marker"
+                :style="{ '--marker-color': color }"
+            />
+        </div>
+        <div class="legend-item__label">{{ text }}</div>
+        <div class="legend-item__counter">{{ counter }}</div>
     </div>
-    <div class="legend-item__label">{{ text }}</div>
-    <div class="legend-item__counter">{{ counter }}</div>
-  </div>
 </template>
 
 <script>
@@ -29,25 +32,25 @@ export default {
 
 <style scoped>
 .legend-item {
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 }
 
 .legend-item .legend-item__marker .marker {
-  width: 18px;
-  height: 18px;
-  background-color: var(--marker-color);
-  border: 1px solid #bababa;
-  border-radius: 5px;
+    width: 18px;
+    height: 18px;
+    background-color: var(--marker-color);
+    border: 1px solid #bababa;
+    border-radius: 5px;
 }
 
 .legend-item .legend-item__label {
-  flex-basis: 100%;
-  margin: 0 24px;
+    flex-basis: 100%;
+    margin: 0 24px;
 }
 
 .legend-item .legend-item__counter {
-  color: #8e8e8e;
-  font-size: 0.9rem;
+    color: #8e8e8e;
+    font-size: 0.9rem;
 }
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,18 +2,92 @@
     <div class="map">
         <h3>Карта офиса</h3>
 
-        <div class="map-root">
-            <MapSVG />
+        <div
+            v-if="!isLoading"
+            class="map-root"
+        >
+            <MapSVG ref="svg" />
+            <Table
+                v-show="false"
+                ref="table"
+            />
         </div>
+        <div v-else>Loading...</div>
     </div>
 </template>
 
 <script>
+import * as d3 from 'd3';
+
 import MapSVG from '@/assets/map.svg';
+import Table from '@/assets/workPlace.svg';
+
+import tables from "@/assets/tables.json";
 
 export default {
     components: {
         MapSVG,
+        Table
+    },
+    data() {
+        return {
+            isLoading: false,
+            svg: null,
+            g: null,
+            tableSVG: null,
+            tables: []
+        }
+    },
+    created () {
+        this.tables = tables;
+    },
+    mounted () {
+        this.isLoading = false;
+
+        this.svg = d3.select(this.$refs.svg);
+        this.g = this.svg.select('g');
+        this.tableSVG = d3.select(this.$refs.table);
+        if (this.g) {
+            this.drawTables();
+        } else {
+            alert('SVG is incorrect')
+        }
+
+        this.isLoading = false;
+    },
+    methods: {
+        drawTables() {
+            // создаем группу для рабочик мест
+            const svgTablesGroupPlace = this.g // ??? почему пустой g пропадает в браузере
+                .append('g')
+                .attr('x', 0)
+                .attr('y', 0)
+                .classed('groupPlaces', true);
+
+            this.tables.map(table => {
+                // создает группу для рабочего стола
+                const targetSeat = svgTablesGroupPlace
+                    .append('g')
+                    .attr('transform', `translate(${table.x}, ${table.y}) scale(0.5)`) // ??? почему недостаточно x y
+                    .attr('x', `${table.x}`)
+                    .attr('y', `${table.y}`)
+                    .attr('id', `place_${table._id}`)
+                    .classed('employer-place', true);
+
+                // устанавливает стол в группу
+                targetSeat
+                    .append('g')
+                    .attr(
+                        'transform',
+                        `rotate(${table.rotate || 0})`
+                    )
+                    .classed('table', true)
+                    .html(this.tableSVG.html())
+                    .style('fill', table.color ?? 'transparent')
+                    .attr('width', '50px')
+                    .attr('height', '50px');
+            });
+        }
     },
 };
 </script>
